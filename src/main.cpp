0558495218a3bcf9853aa90021f64fa24efc9b32
dheijl <danny.heijl@telenet.be>
2025-12-06 17:01:58 +0100

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -41,10 +41,6 @@ void setup()
     M5.Power.setExtOutput(false, m5::ext_port_mask_t(255));
     WiFi.setPins(SDIO2_CLK, SDIO2_CMD, SDIO2_D0, SDIO2_D1, SDIO2_D2, SDIO2_D3, SDIO2_RST);
 
-    esp_pm_config_t config = { 360, 40, true };
-    esp_pm_configure(&config);
-
-
     Serial.begin(115200);
     // display 
     M5.Display.setRotation(0);
@@ -55,9 +51,20 @@ void setup()
     M5.Display.setTextScroll(true);
     M5.Display.waitDisplay();
     M5.Display.setBrightness(40);
+
+    // display battery/power state on top line 
     display_power_ui();
 
-    // initialize network from SD if SD present
+    // check battery low
+    M5.Display.setCursor(0, 64);
+    auto bat_info = get_power();
+    if ((bat_info.bat_level < 20) && !(bat_info.ext_power | bat_info.is_charging)) {
+        M5.Display.println("Battery low - please connect external power.");
+        vTaskDelay(2000);
+        M5.Power.powerOff();
+    }
+
+    // initialize NVS network config from SD if SD present
     NETWORK_CFG nw_cfg;
     if (SD_Config::read_wifi(nw_cfg)) {
         log_d("saving wifi to NVS: %s %s", nw_cfg.ssid, nw_cfg.psw);
@@ -65,13 +72,14 @@ void setup()
             log_d("error writing NVS wifi!");
         }
     }
-
+    // start wifi connection and show on topline 
     connect_wifi();
     check_time();
 
+    // display datetime on second line, using SNTP sync once a day
     display_date_time_ui();
 
-    // initialize player config from SD if SD present
+    // initialize NVS player config from SD if SD present 
     // needs WIFI for MDNS lookup
     MPD_PLAYER player;
     if (SD_Config::read_player(player)) {
@@ -81,22 +89,16 @@ void setup()
         }
     }
 
-
-    M5.Display.setCursor(0, 64);
-    auto bat_info = get_power();
-    if ((bat_info.bat_level < 20) && !(bat_info.ext_power | bat_info.is_charging)) {
-        M5.Display.println("Battery low - please connect external power.");
-        vTaskDelay(2000);
-        M5.Power.powerOff();
-    }
+    // connect to MPD player and show status
     if (! read_player(mpd_pl)) {
         mpd_pl.player_name = "NVS error";
         mpd_pl.player_hostname = "NVS error";
         mpd_pl.player_ip = "0.0.0.0";
         mpd_pl.player_port = 0;
+    } else {
+        MPD_Client mpd(mpd_pl);
+        display_mpd_ui(&mpd);
     }
-    MPD_Client mpd(mpd_pl);
-    display_mpd_ui(&mpd);
     // show current NRG P1 dongle data
     if (p1_request(dongle_data)) {
         display_dongle_ui(dongle_data);
