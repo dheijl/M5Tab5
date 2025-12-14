6d2525117c6fa31b7db2f4495d731375f9bdb19b
dheijl <danny.heijl@telenet.be>
2025-12-14 20:34:16 +0100

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -20,13 +20,21 @@ static m5::rtc_datetime_t now;
 static MPD_PLAYER mpd_pl;
 static P1_DATA dongle_data;
 
-static void log_ram()
-{
-    log_d("Total heap: %d", ESP.getHeapSize());
-    log_d("Free heap: %d", ESP.getFreeHeap());
-    log_d("Total PSRAM: %d", ESP.getPsramSize());
-    log_d("Free PSRAM: %d", ESP.getFreePsram());
-}
+typedef struct task_timers {
+    bool sec_elapsed;
+    bool min_elapsed;
+    bool five_min_elapsed;
+    bool display_timer_elapsed;
+} TASK_TIMERS;
+
+const uint32_t DISPLAY_TIME_SECS = 7; // seconds display stays on after touch
+
+static TASK_TIMERS UpdateTimers(bool sleeping);
+static bool CheckTouch();
+static bool UpdateStatus(const TASK_TIMERS& tasktimers);
+static void CheckPowerOff();
+static void ShowData();
+static void log_ram();
 
 void setup()
 {
@@ -62,7 +70,7 @@ void setup()
     NETWORK_CFG nw_cfg;
     if (SD_Config::read_wifi(nw_cfg)) {
         log_d("saving wifi to NVS: %s %s", nw_cfg.ssid, nw_cfg.psw);
-        if (! write_wifi(nw_cfg)) {
+        if (!write_wifi(nw_cfg)) {
             log_d("error writing NVS wifi!");
         }
     }
@@ -84,52 +92,35 @@ void setup()
     }
 
     // connect to MPD player and show status
-    if (! read_player(mpd_pl)) {
+    if (!read_player(mpd_pl)) {
         mpd_pl.player_name = "NVS error";
         mpd_pl.player_hostname = "NVS error";
         mpd_pl.player_ip = "0.0.0.0";
         mpd_pl.player_port = 0;
-    } else {
-        MPD_Client mpd(mpd_pl);
-        display_mpd_ui(&mpd);
     }
-    // show current NRG P1 dongle data
-    if (p1_request(dongle_data)) {
-        display_dongle_ui(dongle_data);
+    else {
+        ShowData();
     }
 
     M5.Display.setCursor(0, 64);
 
 }
 
-typedef struct task_timers {
-    bool sec_elapsed;
-    bool min_elapsed;
-    bool five_min_elapsed;
-} TASK_TIMERS;
-
-const uint32_t DISPLAY_TIME_SECS = 7; // seconds display stays on after touch
-
-static void UpdateTimers(TASK_TIMERS& tasktimers);
-static bool CheckTouch();
-static bool UpdateDisplay(const TASK_TIMERS& tasktimers);
-static void CheckPowerOff();
 
 void loop()
 {
     static bool sleeping = false;
 
-    TASK_TIMERS tasktimers{ false, false, false };
-
+    // update timers
+    TASK_TIMERS tasktimers = UpdateTimers(sleeping);
     // check touchscreen
     if (CheckTouch()) {
         sleeping = false;
+        ShowData();
     }
-    // update timers
-    UpdateTimers(tasktimers);
-    // if touched: update display while not sleeping
+    // if touched: update time/power status while not sleeping
     if (!sleeping) {
-        sleeping = UpdateDisplay(tasktimers);
+        sleeping = UpdateStatus(tasktimers);
     }
     // every 5 mins: check if on battery and not playing => poweroff
     if (tasktimers.five_min_elapsed) CheckPowerOff();
@@ -138,25 +129,24 @@ void loop()
 }
 
 /// @brief CheckPowerOff: check every 5 minutes if poweroff desired
-/// @param tasktimers 
 static void CheckPowerOff()
 {
-        auto bi = get_power();
-        // battery low and not charging
-        if (bi.bat_level < 20 && !bi.is_charging) {
-            WiFi.disconnect();
-            M5.Power.powerOff();
-        }
-        WiFi.setSleep(false);
-        MPD_Client mpd(mpd_pl);
-        // not playing and not on external power
-        if (!mpd.is_playing() && (bi.bat_current > 10)) {
-            WiFi.disconnect();
-            M5.Power.powerOff();
-        }
-        else {
-            WiFi.setSleep(true);
-        }
+    auto bi = get_power();
+    // battery low and not charging
+    if (bi.bat_level < 20 && !bi.is_charging) {
+        WiFi.disconnect();
+        M5.Power.powerOff();
+    }
+    WiFi.setSleep(false);
+    MPD_Client mpd(mpd_pl);
+    // not playing and not on external power
+    if (!mpd.is_playing() && (bi.bat_current > 10)) {
+        WiFi.disconnect();
+        M5.Power.powerOff();
+    }
+    else {
+        WiFi.setSleep(true);
+    }
 }
 
 /// @brief check if touchscreen pressed
@@ -167,64 +157,75 @@ static bool CheckTouch()
     auto touch = M5.Touch.getDetail();
     if (touch.wasPressed()) {
         touched = true;
-        // update power display (values without wifi and display)
-        display_power_ui();
-        // prepare WiFi and display 
-        WiFi.setSleep(false);
-        M5.Display.powerSaveOff();
-        M5.Display.setBrightness(10);
-        if (WiFi.status() != WL_CONNECTED) {
-            connect_wifi();
-        }
-        // show mpd status
-        MPD_Client mpd(mpd_pl);
-        display_mpd_ui(&mpd);
-        // show P1 meter dongle data 
-        if (p1_request(dongle_data)) {
-            display_dongle_ui(dongle_data);
-        }
-        WiFi.setSleep(true);
     }
     return touched;
 }
 
-/// @brief the display is showing: update as necessary and go to sleep if timer expired
-/// @param tasktimers 
-static bool UpdateDisplay(const TASK_TIMERS& tasktimers)
-{
-    static uint32_t display_timer = 0;
+/// @brief show MPD and P1-Dongle data
+static void ShowData() {
+    // update power display (values without wifi and display)
+    display_power_ui();
+    // prepare WiFi and display 
+    WiFi.setSleep(false);
+    M5.Display.powerSaveOff();
+    M5.Display.setBrightness(10);
+    if (WiFi.status() != WL_CONNECTED) {
+        connect_wifi();
+    }
+    // show mpd status
+    MPD_Client mpd(mpd_pl);
+    display_mpd_ui(&mpd);
+    // show P1 meter dongle data 
+    if (p1_request(dongle_data)) {
+        display_dongle_ui(dongle_data);
+    }
+    WiFi.setSleep(true);
+}
 
-    bool sleeping = false;
+/// @brief the display is showing: update as necessary and enter sleep mode if necessary
+/// @param tasktimers 
+/// @returns bool: enter sleep or not yet
+static bool UpdateStatus(const TASK_TIMERS& tasktimers)
+{
+
     if (tasktimers.sec_elapsed) {
-        display_timer++;
         display_date_time_ui();
     }
     if (tasktimers.min_elapsed) {
         display_power_ui();
     }
-    if (display_timer >= DISPLAY_TIME_SECS) {  
+    if (tasktimers.display_timer_elapsed) {
         M5.Display.setBrightness(0);
         WiFi.setSleep(true);
         M5.Display.powerSaveOn();
-        sleeping = true;
-        display_timer = 0;
         log_ram();
+        return true;
     }
-    return sleeping;
+    return false;
 }
 
 /// @brief update the timers that say what to do
 /// @param tasktimers 
-static void UpdateTimers(TASK_TIMERS& tasktimers)
+static TASK_TIMERS UpdateTimers(bool sleeping)
 {
     static int8_t last_second = 0;
     static int8_t last_minute = 0;
     static uint32_t minutes_elapsed = 0;
+    static uint32_t display_timer = 0;
+
+    TASK_TIMERS tasktimers = { false, false, false, false };
 
     now = M5.Rtc.getDateTime();
     if (now.time.seconds != last_second) {
         tasktimers.sec_elapsed = true;
         last_second = now.time.seconds;
+        if (!sleeping) {
+            display_timer++;
+            if (display_timer >= DISPLAY_TIME_SECS) {
+                tasktimers.display_timer_elapsed = true;
+                display_timer = 0;
+            }
+        }
     }
     if (now.time.minutes != last_minute) {
         minutes_elapsed++;
@@ -235,5 +236,14 @@ static void UpdateTimers(TASK_TIMERS& tasktimers)
             tasktimers.five_min_elapsed = true;
         }
     }
+    return tasktimers;
 }
- 
+
+/// @brief show relevant RAM/PSRAM size
+static void log_ram()
+{
+    log_d("Total heap: %d", ESP.getHeapSize());
+    log_d("Free heap: %d", ESP.getFreeHeap());
+    log_d("Total PSRAM: %d", ESP.getPsramSize());
+    log_d("Free PSRAM: %d", ESP.getFreePsram());
+}
